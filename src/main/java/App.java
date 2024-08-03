c7c6474bf64357f64ce2fd14621864854725e851
sajimpk <sajimpk520@gmail.com>
2024-08-03 14:34:11 +0600

    gradle base

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
+
